943894cd36ef1653f43c7d0d5e6e00f20fc5d123
Marcin Skruch <mskruch@architech.ca>
2016-05-05 17:46:43 +0200

    Test.

--- src/test/java/solution/DecimalZipTest.java
+++ src/test/java/solution/DecimalZipTest.java
@@ -12,7 +12,6 @@ public class DecimalZipTest
 	@Test
 	public void test()
 	{
-		assertEquals(16273890, new DecimalZip().solution(123, 67890));
 		assertEquals(0, new DecimalZip().solution(0, 0));
 		assertEquals(100_000_000, new DecimalZip().solution(10_000_000, 0));
 		assertEquals(-1, new DecimalZip().solution(10_000_000, 1));
